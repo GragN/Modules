c80bd4082a2883b264a81775d7485ac82f6bf6ef
GragN <98579784+GragN@users.noreply.github.com>
2023-03-07 08:21:49 +0900

    commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html", 
+        filename: 'main.html'}),
+        new MiniCssExtractPlugin()]
+    }
